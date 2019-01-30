54533804e1c94693ffc377deffd6ead857de4088
pbhudiaBAE <46529282+pbhudiaBAE@users.noreply.github.com>
2019-01-30 10:52:45 +0000

    Add files via upload

---
+++ Q2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	i, c, sum := [...]int{1, 1}, 0, 0
+	for c<4e6 {
+		c = i[0] + i[1]; i[0] = i[1]; i[1] = c
+		if c % 2 == 0{
+			sum = sum + c
+		}
+	}
+	fmt.Printf("Sum of all even fibonacci seq terms < 4e6 = %d\n", sum)
+}
